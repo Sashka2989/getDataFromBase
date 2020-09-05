f707e700139f711476e6699dd33d7607878aeca2
Sashka2989 <Sashu998@gmail.com>
2020-09-06 01:21:24 +0700

    Create function for create JSON array of products

--- src/main/java/com/alexplot/getdatafrombase/ParserForStat.java
+++ src/main/java/com/alexplot/getdatafrombase/ParserForStat.java
@@ -2,25 +2,13 @@ package com.alexplot.getdatafrombase;
 
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import javax.xml.crypto.Data;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ParserForStat {
-    private static final String SELECT_QUERY_GET_PRODUCTS =
-            "SELECT products.name, sum(products.price) FROM buyers " +
-                    "INNER JOIN buy " +
-                    "ON buyers.id = buy.id_buyer " +
-                    "INNER JOIN products " +
-                    "ON buy.pruduct_id = products.id " +
-                    "WHERE buyers.id = ? " +
-                    "AND buy.purchase_date BETWEEN ? AND ?" +
-                    "GROUP BY products.name " +
-                    "ORDER BY sum(products.price) DESC";
 
     private static Date startDate;
     private static Date endDate;
@@ -43,6 +31,7 @@ public class ParserForStat {
             connection = DriverManager.getConnection(ConnectionData.URL,
                     ConnectionData.USER, ConnectionData.PASSWORD);
 
+            System.out.println(createJsonProductsArray(connection, 2).toJSONString());
 
 
 
@@ -60,6 +49,47 @@ public class ParserForStat {
 
         return resultObject;
     }
+
+    private static JSONArray createJsonProductsArray(Connection connection, long idPerson) {
+        JSONArray productJsonArray = new JSONArray();
+
+        final String SELECT_QUERY_GET_PRODUCTS =
+                "SELECT products.name, sum(products.price) FROM buyers " +
+                        "INNER JOIN buy " +
+                        "ON buyers.id = buy.id_buyer " +
+                        "INNER JOIN products " +
+                        "ON buy.pruduct_id = products.id " +
+                        "WHERE buyers.id = ? " +
+                        "AND buy.purchase_date BETWEEN ? AND ?" +
+                        "GROUP BY products.name " +
+                        "ORDER BY sum(products.price) DESC";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(SELECT_QUERY_GET_PRODUCTS);
+            statement.setLong(1, idPerson);
+            statement.setDate(2, startDate);
+            statement.setDate(3, endDate);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                JSONObject productJsonObject = new JSONObject();
+
+                productJsonObject.put("name", resultSet.getString("name"));
+                productJsonObject.put("expenses", resultSet.getLong("sum"));
+
+                productJsonArray.add(productJsonObject);
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("Error query");
+            e.printStackTrace();
+        }
+
+        return productJsonArray;
+    }
+
 
     private static int getCountDayBetweenDates(String startDate, String endDate) {
         LocalDate startDt = LocalDate.parse(startDate);
